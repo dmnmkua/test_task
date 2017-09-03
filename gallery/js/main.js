3383d9fb4740d3d393f8dac28eb4e1b4e265c689
dmnmkua <dmn.mk.ua@gmail.com>
2017-09-03 12:12:02 +0300

    add search-field for hidden btn in test-task

--- gallery/js/main.js
+++ gallery/js/main.js
@@ -2,9 +2,11 @@ window.onload = function() {
   var hiddenBtn = document.querySelector("#hidden-btn");
   var header = document.querySelector("#header");
   var icon = document.querySelector('#hidden-btn-icon');
+  var searchField = document.querySelector("#search-wrap");
 
   hiddenBtn.addEventListener("click", function() {
     header.classList.toggle("header--open");
     icon.classList.toggle('hidden-btn__icon--transform');
+    searchField.classList.toggle("search-wrap--show");
   })
 }
